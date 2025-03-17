a172024d9e82dc1d24767dfffa9a8d6b668c3edf
THEpranavsomalraju <somalrajupc@gmail.com>
2025-03-17 00:19:23 -0400

    message

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: ['https://fastidious-youtiao-5b1c11.netlify.app/', 'https://shiplet.my', 'https://www.shiplet.my'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'API-Key']}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
